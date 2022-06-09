dfc4bec438e969e005d54c102df3b8ed86f1ff0d
sSsaAa3 <103163095+sSsaAa3@users.noreply.github.com>
2022-06-09 17:05:51 -0500

    Add files via upload

---
+++ a.php
@@ -0,0 +1,45 @@
+<html>
+<head><title>P.H.P.</title></head>
+<body bgcolor= "#A9DFBF">
+<font color= "#ABB2B9"> P.H.P. </font> <br><br>
+
+<p>
+<?php
+$op=$_P['elemento'];
+
+echo ("valor elegido: $op ! <br><br>");
+
+if ($op<=3)
+  {print ("personajes de peliculas");}
+else
+{print ("personajes de series");}
+
+echo ("<br><br>");
+
+switch($op)
+{
+case 1:
+echo("<img src='res.png'>");
+break;
+case 2:
+echo("<img src='dav.png'>");
+break;
+case 3:
+echo("<img src='in.png'>");
+break;
+case 4:
+echo("<img src='lo.png'>");
+break;
+case 5:
+echo("<img src='she.png'>");
+break;
+case 6:
+echo("<img src='ri.png'>");
+break;
+}
+?> </p>
+
+<p>
+
+</body>
+</html>
